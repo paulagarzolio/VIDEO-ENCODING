582f3831337a8e97a325c24c78605ff8ca2205d3
paulagarzolio <77208536+paulagarzolio@users.noreply.github.com>
2021-11-17 13:10:05 +0100

    Update EX3P1.py

--- P1/EX3P1.py
+++ P1/EX3P1.py
@@ -5,10 +5,10 @@ images = list()
 files = os.listdir(os. getcwd())
 
 for file in files:
-    if imghdr.what(file)!= None:
-        images.append(file)
+    if os.path.isfile(file):
+        if imghdr.what(file)!= None and os.path.isfile(file):
+            images.append(file)
 
-print(images)
 print("Select the image from the list of possible images in your current location: ")
 for i, x in enumerate(images):
     print('{0}. {1}'.format(i, repr(x)))
@@ -17,3 +17,5 @@ image_Selected = images[int(input("ENTER: "))]
 print(image_Selected)
 
 subprocess.call(['ffmpeg', '-i', str(image_Selected), "-vf", "format=gray",'BWImage.jpeg'])
+print("Image saved as: ", 'BWImage.jpeg')
+
