cd94d6ac444dbc6f165c68c1005ec0b1c477ae02
paulagarzolio <77208536+paulagarzolio@users.noreply.github.com>
2021-11-21 16:15:47 +0100

    Update EX1P1.py

--- P1/EX1P1.py
+++ P1/EX1P1.py
@@ -1,15 +1,15 @@
 from functions import RGBtoYUV, YUVtoRGB
 import os
-print(os.getcwd())
-option= int(input("Enter 1 if you want to translate from YUV to RGB: \nEnter 2 if you want to translate from RGB to YUV:"))
-a= int(input("First coordinate: "))
-b= int(input("Second coordinate: "))
-c= int(input("Third coordinate: "))
+def EX1P1():
+    option= int(input("Enter 1 if you want to translate from YUV to RGB: \nEnter 2 if you want to translate from RGB to YUV:"))
+    a= int(input("First coordinate: "))
+    b= int(input("Second coordinate: "))
+    c= int(input("Third coordinate: "))
 
-if option==1:
-    print("RGB coordinates= " ,YUVtoRGB(a,b,c))
+    if option==1:
+        print("RGB coordinates= " ,YUVtoRGB(a,b,c))
 
 
-if option==2:
-    print("YUV coordinates= " ,RGBtoYUV(a,b,c))
+    if option==2:
+        print("YUV coordinates= " ,RGBtoYUV(a,b,c))
 
