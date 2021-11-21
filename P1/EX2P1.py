ff1fa6cb3f4b83372140fae134c6871f680ef32e
paulagarzolio <77208536+paulagarzolio@users.noreply.github.com>
2021-11-21 16:16:10 +0100

    Update EX2P1.py

--- P1/EX2P1.py
+++ P1/EX2P1.py
@@ -2,19 +2,20 @@ import os
 import imghdr
 import subprocess
 
-images = list()
-files = os.listdir(os. getcwd())
+def EX2P1():
+    images = list()
+    files = os.listdir(os. getcwd())
 
-for file in files:
-    if os.path.isfile(file):
-        if imghdr.what(file)!= None and os.path.isfile(file):
-            images.append(file)
+    for file in files:
+        if os.path.isfile(file):
+            if imghdr.what(file)!= None and os.path.isfile(file):
+                images.append(file)
 
-print("Select the image from the list of possible images in your current location: ")
-for i, x in enumerate(images):
-    print('{0}. {1}'.format(i, repr(x)))
+    print("Select the image from the list of possible images in your current location: ")
+    for i, x in enumerate(images):
+        print('{0}. {1}'.format(i, repr(x)))
 
-image_Selected = images[int(input("ENTER: "))]
-quality = str(input("Quality value: "))
-subprocess.call(['ffmpeg', '-i',str(image_Selected), '-q:v', quality,'LowQualityImage.jpeg'])
-print("Image saved as: ", 'LowQualityImage.jpeg')
+    image_Selected = images[int(input("ENTER: "))]
+    quality = str(input("Quality value: "))
+    subprocess.call(['ffmpeg', '-i',str(image_Selected), '-q:v', quality,'LowQualityImage.jpeg'])
+    print("Image saved as: ", 'LowQualityImage.jpeg')
