25e50e87285af7c2ca8b844df74b7dd0b3c5a22b
paulagarzolio <77208536+paulagarzolio@users.noreply.github.com>
2021-11-21 16:16:55 +0100

    Update EX4P1.py

--- P1/EX4P1.py
+++ P1/EX4P1.py
@@ -13,12 +13,13 @@ def applyRunLengthEncoding(serie):
             i=1
     encodedSerie.append([char,i])
     return encodedSerie
-try:
-    print("Write the serie you want to encode. \nPlease use quotation marks!! ")
-    serie= (input())
-    print(serie)
-    print("Encoded Serie: ",applyRunLengthEncoding(serie))
-except:
-    print("Error: You should write your input using quotation marks! Try again.")
+def EX4P1():
+    try:
+        print("Write the serie you want to encode. \nPlease use quotation marks!! ")
+        serie= (input())
+        print(serie)
+        print("Encoded Serie: ",applyRunLengthEncoding(serie))
+    except:
+        print("Error: You should write your input using quotation marks! Try again.")
 
 
