06760a43b337e847a3f58b5710939a48622a7e21
paulagarzolio <77208536+paulagarzolio@users.noreply.github.com>
2021-11-23 22:00:40 +0100

    Update EX4P2.py

--- P2/EX4P2.py
+++ P2/EX4P2.py
@@ -15,7 +15,7 @@ def EX4P2():
         print('{0}. {1}'.format(i, repr(
             x)))  # all the possible videos that the user can select will be displayed in the screen with their corresponding number.
     video_Selected = videos[int(input("ENTER: "))]  # here the user inputs the exercise number, and from the list of videos, it chooses the desired one
-    os.system("ffmpeg -i"+ video_Selected+ "-q:a 0 -map a stereo.mp3") #first we get just the audio of the selected video and we store it in the directory
+    os.system("ffmpeg -i "+ video_Selected+ " -q:a 0 -map a stereo.mp3") #first we get just the audio of the selected video and we store it in the directory
     os.system("ffmpeg -i stereo.mp3 -ac 1 mono.mp3")  #now we convert the stereo audio into mono
     os.system("ffmpeg -i mono.mp3 -acodec flac -vcodec copy out.flac") # here the mono audio is converted into another audio codec. Which in this case is .flac
     os.system("ffmpeg -i output.mp4 -i out.flac -c:v copy -map 0:v:0 -map 1:a:0 new.mp4") #lastly, we put the resulting audio again in the original video
