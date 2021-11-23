3db4ae8474e1726f0b2ab39dc2c9a70ae0d6fa0c
paulagarzolio <77208536+paulagarzolio@users.noreply.github.com>
2021-11-23 19:41:24 +0100

    Update main.py

--- P2/main.py
+++ P2/main.py
@@ -1,5 +1,10 @@
 import EX1P2, EX2P2, EX3P2, EX4P2
-
+print("EXERCICES: ")
+exercices=list(["Cut N seconds a video.","Create a new video with both the YUV histogram of a video and the video."
+               , "Resize video into 4 different video outputs.","Change audio of video into mono output and in a different audio codec."])
+for i, x in enumerate(exercices):
+    print('{0}. {1}'.format(i+1, repr(
+        x)))
 option=int(input("Enter the exercise number you want to execute (1,2,3 or 4!): "))
 
 if option==1:
