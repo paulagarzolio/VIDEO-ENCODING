c8a17e1a0a0cb741172c3caab2f56d47a30ff104
paulagarzolio <77208536+paulagarzolio@users.noreply.github.com>
2021-11-23 20:11:40 +0100

    Add files via upload

--- P2/EX1P2.py
+++ P2/EX1P2.py
@@ -8,11 +8,11 @@ def EX1P2():
             if file[-4:] == (".mov") or file[-4:] == (".mp4"):
                 videos.append(file)
 
-    print("Select the image from the list of possible images in your current location: ")
+    print("\nSelect the video from the list of possible videos in your current location: ")
     for i, x in enumerate(videos):
         print('{0}. {1}'.format(i, repr(x))) #all the possible videos that the user can select will be displayed in the screen with their corresponding number.
     video_Selected = videos[int(input("ENTER: "))] #here the user inputs the exercise number, and from the list of videos, it chooses the desired one
-    N=int(input("Length you want your video to have: ")) #here the user inputs the length of the video (how much they want to conserve from the original video
+    N=int(input("Length you want your video to have [seconds]: ")) #here the user inputs the length of the video (how much they want to conserve from the original video
     string= "00:00:"+str(N)
     subprocess.call(['ffmpeg', '-ss','00:00:00','-i', video_Selected, "-to", string,"-c","copy",'output.mp4']) #this is the code used to call the terminal with the ffmpeg instructions
-    print("Image saved as: ", 'output.mp4') #this will show the user the name of the output file
+    print("Video saved as: ", 'output.mp4') #this will show the user the name of the output file
